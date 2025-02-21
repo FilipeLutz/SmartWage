04cd954722b4695060004bdaf8b98356d653518a
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-02-21 00:10:00 +0000

    Update Income.kt

--- app/src/main/java/com/finalproject/smartwage/data/local/entities/Income.kt
+++ app/src/main/java/com/finalproject/smartwage/data/local/entities/Income.kt
@@ -11,5 +11,10 @@ data class Income(
     @get:PropertyName("userId") @set:PropertyName("userId") var userId: String = "",
     @get:PropertyName("source") @set:PropertyName("source") var source: String = "",
     @get:PropertyName("amount") @set:PropertyName("amount") var amount: Double = 0.0,
-    @get:PropertyName("date") @set:PropertyName("date") var date: Long = System.currentTimeMillis()
+    @get:PropertyName("paye") @set:PropertyName("paye") var paye: Double = 0.0,
+    @get:PropertyName("usc") @set:PropertyName("usc") var usc: Double = 0.0,
+    @get:PropertyName("prsi") @set:PropertyName("prsi") var prsi: Double = 0.0,
+    @get:PropertyName("date") @set:PropertyName("date") var date: Long = System.currentTimeMillis(),
+    @get:PropertyName("frequency") @set:PropertyName("frequency") var frequency: String = "",
+    @get:PropertyName("payPeriod") @set:PropertyName("payPeriod") var payPeriod: Int = 0
 )
