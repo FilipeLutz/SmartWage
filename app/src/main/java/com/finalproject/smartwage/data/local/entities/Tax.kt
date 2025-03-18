6083261d52119f4b99be5dfaeb20d68b94c00846
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-18 09:02:30 +0000

    Update Tax.kt

--- app/src/main/java/com/finalproject/smartwage/data/local/entities/Tax.kt
+++ app/src/main/java/com/finalproject/smartwage/data/local/entities/Tax.kt
@@ -10,6 +10,15 @@ data class Tax(
     @PrimaryKey @get:PropertyName("id") @set:PropertyName("id") var id: String = UUID.randomUUID().toString(),
     @get:PropertyName("userId") @set:PropertyName("userId") var userId: String = "",
     @get:PropertyName("totalIncome") @set:PropertyName("totalIncome") var totalIncome: Double = 0.0,
+    @get:PropertyName("totalPAYE") @set:PropertyName("totalPAYE") var totalPAYE: Double = 0.0,
+    @get:PropertyName("totalUSC") @set:PropertyName("totalUSC") var totalUSC: Double = 0.0,
+    @get:PropertyName("totalPRSI") @set:PropertyName("totalPRSI") var totalPRSI: Double = 0.0,
+    @get:PropertyName("tuitionFeeRelief") @set:PropertyName("tuitionFeeRelief") var tuitionFeeRelief: Double = 0.0,
+    @get:PropertyName("rentTaxCredit") @set:PropertyName("rentTaxCredit") var rentTaxCredit: Double = 0.0,
+    @get:PropertyName("expectedPAYE") @set:PropertyName("expectedPAYE") var expectedPAYE: Double = 0.0,
+    @get:PropertyName("expectedUSC") @set:PropertyName("expectedUSC") var expectedUSC: Double = 0.0,
+    @get:PropertyName("expectedPRSI") @set:PropertyName("expectedPRSI") var expectedPRSI: Double = 0.0,
+    @get:PropertyName("currentWeek") @set:PropertyName("currentWeek") var currentWeek: Int = 0,
     @get:PropertyName("taxOwed") @set:PropertyName("taxOwed") var taxOwed: Double = 0.0,
     @get:PropertyName("lastCalculated") @set:PropertyName("lastCalculated") var lastCalculated: Long = System.currentTimeMillis()
 )
