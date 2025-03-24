1fa34014bcbdfbc30ecfe9d182a2cf46a046e922
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-24 02:36:04 +0000

    Update SmartWageDatabase.kt

--- app/src/main/java/com/finalproject/smartwage/data/local/SmartWageDatabase.kt
+++ app/src/main/java/com/finalproject/smartwage/data/local/SmartWageDatabase.kt
@@ -6,15 +6,17 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.finalproject.smartwage.data.local.dao.ExpenseDao
 import com.finalproject.smartwage.data.local.dao.IncomeDao
+import com.finalproject.smartwage.data.local.dao.SettingsDao
 import com.finalproject.smartwage.data.local.dao.TaxDao
 import com.finalproject.smartwage.data.local.dao.UserDao
 import com.finalproject.smartwage.data.local.entities.Expenses
 import com.finalproject.smartwage.data.local.entities.Tax
 import com.finalproject.smartwage.data.local.entities.User
 import com.finalproject.smartwage.data.local.entities.Income
+import com.finalproject.smartwage.data.local.entities.Settings
 
 @Database(
-    entities = [User::class, Income::class, Expenses::class, Tax::class],
+    entities = [User::class, Income::class, Expenses::class, Tax::class, Settings::class],
     version = 1,
     exportSchema = false
 )
@@ -23,6 +25,7 @@ abstract class SmartWageDatabase : RoomDatabase() {
     abstract fun incomeDao(): IncomeDao
     abstract fun expenseDao(): ExpenseDao
     abstract fun taxDao(): TaxDao
+    abstract fun settingsDao(): SettingsDao
 
     companion object {
         @Volatile
