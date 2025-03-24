b90d5e298bf7f2595eea047f2a98e367737df7c5
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-24 00:33:42 +0000

    Update ProfileViewModel.kt

--- app/src/main/java/com/finalproject/smartwage/viewModel/ProfileViewModel.kt
+++ app/src/main/java/com/finalproject/smartwage/viewModel/ProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.finalproject.smartwage.viewModel
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.finalproject.smartwage.data.local.entities.User
@@ -7,6 +8,8 @@ import com.finalproject.smartwage.data.repository.AuthRepository
 import com.finalproject.smartwage.data.repository.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,21 +20,63 @@ class ProfileViewModel @Inject constructor(
     private val auth: FirebaseAuth
 ) : ViewModel() {
 
-    fun updateUser(remoteUser: User) {
+    private val _resetEmailSent = MutableStateFlow<Boolean>(false)
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    private val _user = MutableStateFlow<User?>(null)
+    val user: StateFlow<User?> = _user
+
+    init {
+        getCurrentUser()
+    }
+
+    fun sendPasswordResetEmail(email: String) {
         viewModelScope.launch {
-            userRepo.saveUser(remoteUser)
+            val result = userRepo.sendPasswordResetEmail(email)
+            if (result) {
+                _resetEmailSent.value = true
+            } else {
+                _errorMessage.value = "Failed to send reset email. Please try again."
+            }
         }
     }
 
-    fun deleteUser(userId: String) {
-        viewModelScope.launch {
-            userRepo.logout(userId)
+    // Fetch current user data
+    private fun getCurrentUser() {
+        auth.currentUser?.uid?.let { userId ->
+            viewModelScope.launch {
+                userRepo.getUser(userId, viewModelScope).collect { localUser ->
+                    if (localUser != null) {
+                        _user.value = localUser
+                    }
+                }
+            }
         }
     }
 
-    fun logout() {
+    // Update User Profile
+    fun updateUser(name: String, phoneNumber: String, imageUri: Uri?) {
         viewModelScope.launch {
-            authRepo.logout()
+            val currentUser = _user.value
+            currentUser?.let {
+                val updatedUser = it.copy(
+                    name = name,
+                    phoneNumber = phoneNumber,
+                    profilePicture = imageUri?.toString() ?: it.profilePicture
+                )
+                userRepo.saveUser(updatedUser)
+                _user.value = updatedUser
+            }
+        }
+    }
+
+    // Delete User Account
+    fun deleteUserAccount() {
+        auth.currentUser?.uid?.let { userId ->
+            viewModelScope.launch {
+                userRepo.deleteUserAccount(userId)
+                authRepo.deleteUser()
+                _user.value = null
+            }
         }
     }
 }
