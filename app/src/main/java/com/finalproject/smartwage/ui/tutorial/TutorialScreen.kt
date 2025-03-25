4bb8051309e20c87e14089ed9ae3b3f8d3856a2e
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-25 20:56:39 +0000

    Update TutorialScreen.kt

--- app/src/main/java/com/finalproject/smartwage/ui/tutorial/TutorialScreen.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/tutorial/TutorialScreen.kt
@@ -1,7 +1,10 @@
 package com.finalproject.smartwage.ui.tutorial
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
