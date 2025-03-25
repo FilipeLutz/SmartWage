b76d90de08c95cd7b9515452056dfda8edee515f
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-25 20:55:03 +0000

    Update PhotoDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/PhotoDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/PhotoDialog.kt
@@ -1,15 +1,26 @@
 package com.finalproject.smartwage.ui.components.dialogs
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.finalproject.smartwage.ui.theme.DarkBlue
 
 @Composable
 fun PhotoDialog(
@@ -19,27 +30,75 @@ fun PhotoDialog(
 ) {
     AlertDialog(
         onDismissRequest = onDismiss,
-        title = { Text("Choose an option") },
-        text = { Text("Would you like to take a new picture, choose from the gallery, or delete the current picture?") },
-        confirmButton = {
-            Column {
-                Button(onClick = {
-                    onDismiss()
-                    onCameraClick()
-                }) { Text("Take Photo") }
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "Choose an option",
+                    fontSize = 24.sp,
+                    fontWeight = Bold
+                )
 
-                Spacer(modifier = Modifier.height(4.dp))
-
-                Button(onClick = {
-                    onDismiss()
-                    onGalleryClick()
-                }) { Text("Choose from Gallery") }
+                IconButton(
+                    onClick = onDismiss
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close",
+                        tint = DarkBlue,
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                }
             }
         },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text("Cancel")
+        text = {
+            Text(
+                "Would you like to take a new picture or choose from the gallery?",
+                fontSize = 15.sp,
+                fontWeight = SemiBold
+            )
+        },
+        confirmButton = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = {
+                        onDismiss()
+                        onCameraClick()
+                    },
+                    colors = ButtonDefaults.buttonColors(DarkBlue),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text("Take Photo",
+                        fontSize = 18.sp,
+                        fontWeight = Bold
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        onDismiss()
+                        onGalleryClick()
+                    },
+                    colors = ButtonDefaults.buttonColors(DarkBlue),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Text("Choose from Gallery",
+                        fontSize = 18.sp,
+                        fontWeight = Bold
+                    )
+                }
             }
-        }
+        },
+        dismissButton = {}
     )
 }
