b323bc3e7522883d16d20925776fdfe70fbbfe31
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-26 12:22:51 +0000

    Update TaxResultDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/TaxResultDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/TaxResultDialog.kt
@@ -1,6 +1,8 @@
 package com.finalproject.smartwage.ui.components.dialogs
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,18 +13,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration.Companion.Underline
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.navigation.NavController
 import com.finalproject.smartwage.ui.theme.DarkBlue
 import java.text.NumberFormat
 import java.util.Locale
@@ -30,6 +36,7 @@ import java.util.Locale
 @SuppressLint("DefaultLocale")
 @Composable
 fun TaxResultDialog(
+    navController: NavController,
     calculatedTax: Triple<Double, Double, Double>?,
     onDismiss: () -> Unit
 ) {
@@ -72,7 +79,7 @@ fun TaxResultDialog(
                     modifier = Modifier.fillMaxWidth()
                 )
 
-                Spacer(modifier = Modifier.height(2.dp))
+                Spacer(modifier = Modifier.height(5.dp))
 
                 Column (
                     modifier = Modifier
@@ -80,99 +87,36 @@ fun TaxResultDialog(
                         .fillMaxWidth(),
                 ){
 
-                    // Updated Tax Explanation
                     Text(
                         text = "Tax Rate Information",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.DarkGray,
-                        modifier = Modifier.fillMaxWidth()
+                        fontSize = 22.sp,
+                        color = DarkBlue,
+                        fontWeight = SemiBold,
+                        textDecoration = Underline,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable(
+                                onClick = {
+                                    onDismiss()
+                                    navController.navigate("taxcredit")
+                                },
+                                indication = null,
+                                interactionSource = remember { MutableInteractionSource() }
+                            ),
+                        textAlign = TextAlign.Center
                     )
 
-                    Spacer(modifier = Modifier.height(15.dp))
+                    Spacer(modifier = Modifier.height(25.dp))
 
                     Text(
-                        text = "• PAYE (Pay As You Earn)",
-                        fontSize = 20.sp,
-                        color = DarkBlue,
-                        fontWeight = FontWeight.SemiBold
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(
-                        text = "Applied at 20% on the first €44,000 of income, and 40% on any income above it.",
-                        fontSize = 18.sp,
-                        color = Color.DarkGray,
-                        fontWeight = FontWeight.SemiBold,
-                        style = TextStyle(lineHeight = 23.sp)
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(
-                        text = "• USC (Universal Social Charge)",
-                        fontSize = 20.sp,
-                        color = DarkBlue,
-                        fontWeight = FontWeight.SemiBold
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(
-                        "   - 0.5% on the first €12,012",
-                        fontSize = 18.sp, color = Color.Black
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        "   - 2% on the next €15,370",
-                        fontSize = 18.sp, color = Color.Black
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        "   - 3% on the next €42,662",
-                        fontSize = 18.sp, color = Color.Black
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        "   - 8% on anything above €70,044",
-                        fontSize = 18.sp, color = Color.Black
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(
-                        text = "• PRSI \n  (Pay Related Social Insurance)",
-                        fontSize = 20.sp,
-                        color = DarkBlue,
-                        fontWeight = FontWeight.SemiBold,
-                        style = TextStyle(lineHeight = 23.sp)
-                    )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Text(
-                        text = "Charged at 4.1% on incomes above €18,304 annually.",
-                        fontSize = 18.sp,
-                        color = Color.DarkGray,
-                        fontWeight = FontWeight.SemiBold,
-                        style = TextStyle(lineHeight = 23.sp)
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(
-                        text = "• STANDARD TAX CREDIT",
-                        fontSize = 20.sp,
-                        color = DarkBlue,
-                        fontWeight = FontWeight.SemiBold
-                    )
-                    Spacer(modifier = Modifier.height(5.dp))
-                    Text(
-                        text = "Reduces your total income tax liability by €4,000.",
-                        fontSize = 18.sp,
-                        color = Color.DarkGray,
-                        fontWeight = FontWeight.SemiBold,
-                        style = TextStyle(lineHeight = 25.sp)
-                    )
-
-                    Spacer(modifier = Modifier.height(15.dp))
-
-                    Text(
-                        text = "• Please note, this is a basic tax estimate. For a more precise calculation, enter your actual income and expenses in the app.",
+                        text = "Please note, this is a simple tax estimate. For a more precise calculation, enter your actual income and expenses in the app.",
                         fontSize = 18.sp,
                         color = Color.Blue,
-                        fontWeight = FontWeight.SemiBold
+                        fontWeight = SemiBold
                     )
                 }
+
+                Spacer(modifier = Modifier.height(2.dp))
 
                 Row(
                     modifier = Modifier
@@ -182,6 +126,7 @@ fun TaxResultDialog(
                 ) {
                     Button(
                         onClick = onDismiss,
+                        colors = ButtonDefaults.buttonColors(DarkBlue),
                         modifier = Modifier
                             .width(80.dp)
                             .height(50.dp)
