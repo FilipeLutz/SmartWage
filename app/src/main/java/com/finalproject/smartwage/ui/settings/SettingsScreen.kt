7197d3e3bf0c32b307be515dee7fbbfe43a0d6c2
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-26 18:08:58 +0000

    Update SettingsScreen.kt

--- app/src/main/java/com/finalproject/smartwage/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/settings/SettingsScreen.kt
@@ -1,9 +1,14 @@
 package com.finalproject.smartwage.ui.settings
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,169 +18,297 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.finalproject.smartwage.R
 import com.finalproject.smartwage.navigation.Destinations
 import com.finalproject.smartwage.ui.components.DashboardBottomBar
 import com.finalproject.smartwage.ui.components.DashboardTopBar
+import com.finalproject.smartwage.ui.theme.DarkBlue
+import com.finalproject.smartwage.ui.theme.Red
+import com.finalproject.smartwage.ui.theme.White
+import com.finalproject.smartwage.viewModel.AuthViewModel
 import com.finalproject.smartwage.viewModel.SettingsViewModel
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun SettingsScreen(
     navController: NavController,
+    authViewModel: AuthViewModel = hiltViewModel(),
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
-    // Observe the settings from ViewModel
-    val notificationsEnabled by viewModel.notificationsEnabled.collectAsState()
-    val darkModeEnabled by viewModel.darkModeEnabled.collectAsState()
-    val selectedLanguage by viewModel.selectedLanguage.collectAsState()
 
-    var showAboutDialog by remember { mutableStateOf(false) }
-    val languages = listOf("English", "Portuguese", "Spanish")
+    val context = LocalContext.current
+
+    // Observe settings
+    val darkModeEnabled by viewModel.darkModeEnabled.collectAsState()
 
     Scaffold(
         topBar = { DashboardTopBar(navController) },
         bottomBar = { DashboardBottomBar(navController) }
     ) { paddingValues ->
-        Column(
+        Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
                 .background(MaterialTheme.colorScheme.background)
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
-            // Notifications setting
-            SettingItem(title = "Notifications", icon = Icons.Default.Notifications) {
-                Switch(
-                    checked = notificationsEnabled,
-                    onCheckedChange = { viewModel.notificationsEnabled }
-                )
-            }
+                Spacer(modifier = Modifier.height(10.dp))
 
-            HorizontalDivider()
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
 
-            // Change Language setting
-            SettingItem(title = "Change Language", icon = Icons.Default.Edit) {
-                var expanded by remember { mutableStateOf(false) }
-                Box {
-                    Text(selectedLanguage, modifier = Modifier.clickable { expanded = true })
-                    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
-                        languages.forEach { language ->
-                            DropdownMenuItem(
-                                text = { Text(language) },
-                                onClick = {
-                                    viewModel.selectedLanguage
-                                    expanded = false
-                                }
+                    Text(
+                        text = "Settings",
+                        fontSize = 35.sp,
+                        fontWeight = Bold,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Column(modifier = Modifier.fillMaxSize()) {
+
+                    HorizontalDivider()
+
+                    // Language selection
+                    SettingItem(
+                        title = "Language",
+                        icon = painterResource(id = R.drawable.language),
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.End
+                        ){
+                            Image(
+                                painter = painterResource(id = R.drawable.english),
+                                contentDescription = "Language Flag",
+                                modifier = Modifier.size(35.dp)
                             )
+
+                            Spacer(modifier = Modifier.width(6.dp))
                         }
                     }
-                }
-            }
 
-            HorizontalDivider()
+                    HorizontalDivider()
 
-            // FAQs navigation
-            SettingItem(title = "FAQs", icon = Icons.Default.Info) {
-                IconButton(onClick = { navController.navigate(Destinations.Help.route) }) {
-                    Icon(Icons.Default.ArrowForward, contentDescription = "Go to FAQs")
-                }
-            }
-
-            HorizontalDivider()
-
-            // Tutorial navigation
-            SettingItem(title = "Tutorial", icon = Icons.Default.PlayArrow) {
-                IconButton(onClick = { navController.navigate(Destinations.Tutorial.route) }) {
-                    Icon(Icons.Default.ArrowForward, contentDescription = "Go to Tutorial")
-                }
-            }
-
-            HorizontalDivider()
-
-            // Dark Mode setting
-            SettingItem(title = "Dark Mode", icon = Icons.Default.Edit) {
-                Switch(
-                    checked = darkModeEnabled,
-                    onCheckedChange = { viewModel.darkModeEnabled }
-                )
-            }
-
-            HorizontalDivider()
-
-            // About the App setting
-            SettingItem(title = "About the App", icon = Icons.Default.Info) {
-                IconButton(onClick = { showAboutDialog = true }) {
-                    Icon(Icons.Default.ArrowForward, contentDescription = "About the App")
-                }
-            }
-
-            // Show About dialog
-            if (showAboutDialog) {
-                AlertDialog(
-                    onDismissRequest = { showAboutDialog = false },
-                    title = { Text("About the App") },
-                    text = {
-                        Column {
-                            Text("Version: 1.0.0")
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Text("This app provides information about tax and revenue, along with tutorials to guide users.")
-                        }
-                    },
-                    confirmButton = {
-                        TextButton(onClick = { showAboutDialog = false }) {
-                            Text("OK")
-                        }
+                    // Dark Mode setting
+                    SettingItem(
+                        title = "Dark Mode",
+                        icon = painterResource(id = R.drawable.mode),
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Switch(
+                            colors = SwitchDefaults.colors(
+                                checkedThumbColor = White,
+                                checkedTrackColor = DarkBlue,
+                                uncheckedThumbColor = DarkBlue,
+                            ),
+                            checked = darkModeEnabled,
+                            onCheckedChange = { enabled ->
+                                viewModel.updateDarkModeEnabled(enabled)
+                            }
+                        )
                     }
-                )
+
+                    HorizontalDivider()
+
+                    // Profile setting
+                    SettingItem(
+                        title = "Profile",
+                        icon = painterResource(id = R.drawable.user),
+                        onClick = { navController.navigate(Destinations.Profile.route) },
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowforward),
+                            contentDescription = "Profile Settings",
+                            tint = DarkBlue,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+
+                    HorizontalDivider()
+
+                    // Notifications setting
+                    SettingItem(
+                        title = "Notifications",
+                        icon = painterResource(id = R.drawable.notifications),
+                        onClick = { openNotificationSettings(context) },
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowforward),
+                            contentDescription = "Notification Settings",
+                            tint = DarkBlue,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+
+                    HorizontalDivider()
+
+                    // Tutorial navigation
+                    SettingItem(
+                        title = "App Tutorial",
+                        icon = painterResource(id = R.drawable.tutorial),
+                        onClick = { navController.navigate(Destinations.Tutorial.route) },
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowforward),
+                            contentDescription = "Go to Tutorial",
+                            tint = DarkBlue,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+
+                    HorizontalDivider()
+
+                    SettingItem(
+                        title = "Revenue IE",
+                        icon = painterResource(id = R.drawable.faq),
+                        onClick = {
+                            val intent = Intent(Intent.ACTION_VIEW).apply {
+                                data = "https://www.revenue.ie/en/home.aspx".toUri()
+                            }
+                            context.startActivity(intent)
+                        },
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowforward),
+                            contentDescription = "Revenue.ie",
+                            tint = DarkBlue,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+
+                    HorizontalDivider()
+
+                    // About the app
+                    SettingItem(
+                        title = "About App",
+                        icon = painterResource(id = R.drawable.info),
+                        onClick = { navController.navigate(Destinations.AboutApp.route) },
+                        titleColor = DarkBlue,
+                        iconColor = DarkBlue
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowforward),
+                            contentDescription = "About the App",
+                            tint = DarkBlue,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+
+                    HorizontalDivider()
+
+                    // logout
+                    SettingItem(
+                        title = "Log Out",
+                        icon = painterResource(id = R.drawable.logout),
+                        onClick = {
+                            authViewModel.logout()
+                            navController.navigate(Destinations.Login.route) {
+                                popUpTo(Destinations.Login.route) { inclusive = true }
+                            }
+                        },
+                        titleColor = Red,
+                        iconColor = Red
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.arrowforward),
+                            contentDescription = "Logout",
+                            tint = Red,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    }
+                    HorizontalDivider()
+                }
             }
         }
     }
 }
 
 @Composable
-fun SettingItem(title: String, icon: ImageVector, content: @Composable () -> Unit) {
+fun SettingItem(
+    title: String,
+    icon: Painter,
+    titleColor: Color,
+    iconColor: Color,
+    onClick: () -> Unit = {},
+    content: @Composable (() -> Unit)
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .height(70.dp)
+            .padding(20.dp)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                onClick()
+            },
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(icon, contentDescription = title, modifier = Modifier.size(24.dp))
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(title, fontSize = 18.sp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(icon, contentDescription = title, modifier = Modifier.size(26.dp), tint = iconColor)
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(title, fontSize = 20.sp, color = titleColor)
         }
         content()
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun openNotificationSettings(context: Context) {
+    val intent = Intent(android.provider.Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+        putExtra("android.provider.extra.APP_PACKAGE", context.packageName)
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    }
+    context.startActivity(intent)
 }
