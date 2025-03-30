8446e0d5d85357a14c090125b1af5081f6b0ee6d
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-30 20:26:18 +0100

    Update ExpenseRepository.kt

--- app/src/main/java/com/finalproject/smartwage/data/repository/ExpenseRepository.kt
+++ app/src/main/java/com/finalproject/smartwage/data/repository/ExpenseRepository.kt
@@ -14,46 +14,60 @@ import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
+// Class to handle the repository for expenses
 class ExpenseRepository @Inject constructor(
+    // Injecting the required dependencies
     private val incomeDao: ExpenseDao,
     private val firestoreService: FirestoreService,
     private val auth: FirebaseAuth
 ) {
-
+    // Function to save or update expenses
     suspend fun saveOrUpdateExpenses(expenses: Expenses) {
         val currentUser = auth.currentUser ?: return
         val updatedExpenses = expenses.copy(userId = currentUser.uid)
-
+        // Save or update the expenses in Firestore and local database
         withContext(Dispatchers.IO) {
+            // Check if the expense already exists in the local database
             try {
                 firestoreService.saveExpenses(updatedExpenses)
                 incomeDao.insertExpenses(updatedExpenses)
+                // If the expense already exists, update it
             } catch (e: Exception) {
                 Timber.e(e, "Error saving/updating expenses")
             }
         }
     }
 
+    // Function to get user expenses
     fun getUserExpenses(): Flow<List<Expenses>> {
         val currentUser = auth.currentUser
+        // Check if the user is logged in
         return if (currentUser != null) {
+            // Get the user expenses from Firestore and local database
             flow {
+                // Get expenses from Firestore
                 firestoreService.getUserExpenses(currentUser.uid).let { firebaseExpenses ->
+                    // Check if the expenses are not empty
                     if (firebaseExpenses.isNotEmpty()) {
                         emit(firebaseExpenses)
-                    } else {
+                    }
+                    // If the expenses are empty, get them from the local database
+                    else {
                         emit(incomeDao.getUserExpenses(currentUser.uid).firstOrNull() ?: emptyList())
                     }
                 }
             }.flowOn(Dispatchers.IO)
+            // If the user is not logged in, return an empty list
         } else {
             flowOf(emptyList())
         }
     }
 
+    // Function to delete an expense
     suspend fun deleteExpense(expenseId: String) {
         withContext(Dispatchers.IO) {
             try {
+                // Delete the expense from Firestore and local database
                 firestoreService.deleteExpenses(expenseId)
                 incomeDao.deleteExpenses(expenseId)
             } catch (e: Exception) {
