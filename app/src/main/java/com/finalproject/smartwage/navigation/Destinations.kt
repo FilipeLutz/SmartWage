eb722257bb5a1468c18f96d2d222113bdac6fc15
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-30 20:26:35 +0100

    Update Destinations.kt

--- app/src/main/java/com/finalproject/smartwage/navigation/Destinations.kt
+++ app/src/main/java/com/finalproject/smartwage/navigation/Destinations.kt
@@ -1,13 +1,15 @@
 package com.finalproject.smartwage.navigation
 
+// Sealed class Destinations.kt
 sealed class Destinations(val route: String) {
+    // Root destination
     object Login : Destinations("login")
     object SignUp : Destinations("signup")
-
+    // Main destination
     data class Dashboard(val userId: String) : Destinations("dashboard/{userId}") {
         fun createRoute(userId: String) = "dashboard/$userId"
     }
-
+    // Other destinations
     object Income : Destinations("income")
     object Expense : Destinations("expense")
     object TaxCredit : Destinations("taxcredit")
