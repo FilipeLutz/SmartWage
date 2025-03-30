78b82b2524b49d203c3318723ae05c3b5b39cf8d
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-30 20:27:03 +0100

    Update CalendarDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/CalendarDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/CalendarDialog.kt
@@ -2,7 +2,7 @@ package com.finalproject.smartwage.ui.components.dialogs
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,27 +17,36 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+// This code defines a CalendarDialog Composable function that displays a date picker dialog.
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalendarDialog(
+    // Parameters for the CalendarDialog function
     showDialog: Boolean,
     onDismiss: () -> Unit,
     onDateSelected: (String) -> Unit
 ) {
+    // Check if the dialog should be shown
     if (showDialog) {
+        // DatePickerState with the current date
         val datePickerState = rememberDatePickerState(
             initialSelectedDateMillis = System.currentTimeMillis()
         )
-
+        // DatePickerDialog is a Material Design component for selecting a date
         DatePickerDialog(
             onDismissRequest = onDismiss,
             confirmButton = {
+                // Button for confirming the selected date
                 Button(
-                    colors = ButtonDefaults.buttonColors(DarkBlue),
-                    modifier = Modifier.padding(10.dp),
+                    colors = buttonColors(DarkBlue),
+                    modifier = Modifier
+                        .padding(10.dp),
                     onClick = {
+                        // Get the selected date in milliseconds
                         val selectedDate = datePickerState.selectedDateMillis
+                        // Format the selected date to a string
                         if (selectedDate != null) {
+                            // Format the date to "dd-MM-yyyy"
                             val formattedDate = SimpleDateFormat("dd-MM-yyyy", Locale.UK).format(
                                 Date(selectedDate)
                             )
@@ -45,20 +54,32 @@ fun CalendarDialog(
                         }
                         onDismiss()
                     }) {
-                    Text("OK", fontSize = 18.sp)
+                    Text(
+                        "OK",
+                        fontSize = 18.sp
+                    )
                 }
             },
+            // Dismiss button
             dismissButton = {
+                // Button for dismissing the dialog
                 Button(
-                    colors = ButtonDefaults.buttonColors(DarkBlue),
-                    modifier = Modifier.padding(10.dp),
+                    colors = buttonColors(DarkBlue),
+                    modifier = Modifier
+                        .padding(10.dp),
                     onClick = onDismiss
                 ) {
-                    Text("Cancel", fontSize = 18.sp)
+                    Text(
+                        "Cancel",
+                        fontSize = 18.sp
+                    )
                 }
             }
         ) {
-            DatePicker(state = datePickerState)
+            // DatePicker State
+            DatePicker(
+                state = datePickerState
+            )
         }
     }
 }
