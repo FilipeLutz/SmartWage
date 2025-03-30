19815e353eeb83d4d79139b0e23a3b5e09290381
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-30 20:26:20 +0100

    Update IncomeRepository.kt

--- app/src/main/java/com/finalproject/smartwage/data/repository/IncomeRepository.kt
+++ app/src/main/java/com/finalproject/smartwage/data/repository/IncomeRepository.kt
@@ -5,8 +5,8 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import com.finalproject.smartwage.data.local.dao.IncomeDao
-import com.finalproject.smartwage.data.remote.FirestoreService
 import com.finalproject.smartwage.data.local.entities.Income
+import com.finalproject.smartwage.data.remote.FirestoreService
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -16,19 +16,25 @@ import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
 
+// Class to handle income data operations
 class IncomeRepository @Inject constructor(
+    // Dependency injection for IncomeDao and FirestoreService
     private val incomeDao: IncomeDao,
     private val firestoreService: FirestoreService,
     private val auth: FirebaseAuth,
     private val context: Context
 ) {
+    // List to hold offline updates
     private val offlineUpdateQueue = mutableListOf<Income>()
-
+    // Function to save or update income
     suspend fun saveOrUpdateIncome(income: Income) {
+        // Get the current user
         val currentUser = auth.currentUser ?: return
+        // Create a copy of the income object with the user ID
         val updatedIncome = income.copy(userId = currentUser.uid)
-
+        // Check if the user is online
         withContext(Dispatchers.IO) {
+            // If the user is online, save or update the income in Firestore
             try {
                 if (isOnline()) {
 
@@ -51,7 +57,9 @@ class IncomeRepository @Inject constructor(
         }
     }
 
-    fun getUserIncomes(userId: String): Flow<List<Income>> {
+    // Function to get user incomes
+    fun getUserIncomes(): Flow<List<Income>> {
+        // Get the current user
         val currentUser = auth.currentUser
         return if (currentUser != null) {
             flow {
@@ -66,14 +74,7 @@ class IncomeRepository @Inject constructor(
             flowOf(emptyList())
         }
     }
-
-    suspend fun syncOfflineData() {
-        if (isOnline()) {
-            offlineUpdateQueue.forEach { firestoreService.updateIncome(it) }
-            offlineUpdateQueue.clear()
-        }
-    }
-
+    // Function to get offline updates
     @SuppressLint("ServiceCast")
     private fun isOnline(): Boolean {
         val connectivityManager =
@@ -82,7 +83,7 @@ class IncomeRepository @Inject constructor(
             connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         return networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
     }
-
+    // Function to delete income
     suspend fun deleteIncome(incomeId: String) {
         withContext(Dispatchers.IO) {
             try {
