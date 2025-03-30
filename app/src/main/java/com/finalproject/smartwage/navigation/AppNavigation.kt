6b11859cc24b13d5e7e802120b308500b30c2f0c
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-30 20:26:32 +0100

    Update AppNavigation.kt

--- app/src/main/java/com/finalproject/smartwage/navigation/AppNavigation.kt
+++ app/src/main/java/com/finalproject/smartwage/navigation/AppNavigation.kt
@@ -1,7 +1,5 @@
 package com.finalproject.smartwage.navigation
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
@@ -20,41 +18,48 @@ import com.finalproject.smartwage.ui.tax.TaxCreditScreen
 import com.finalproject.smartwage.ui.tutorial.FullVideoScreen
 import com.finalproject.smartwage.ui.tutorial.TutorialScreen
 
-@RequiresApi(Build.VERSION_CODES.O)
+// This file contains the navigation setup for the app.
 @Composable
 fun AppNavigation(
     navController: NavHostController,
     isUserLoggedIn: Boolean,
     userId: String?
 ) {
+    // Set up the navigation host
     NavHost(
         navController = navController,
+        // Start destination based on user login status
         startDestination =
         if (isUserLoggedIn && userId != null)
             Destinations.Dashboard(userId).createRoute(userId)
         else Destinations.Login.route
     ) {
+        // Define the navigation graph
         composable(Destinations.Login.route) { LoginScreen(navController) }
         composable(Destinations.SignUp.route) { SignUpScreen(navController) }
 
+        // Dashboard screen
         composable(
+            // Dynamic route for the dashboard
             route = Destinations.Dashboard("{userId}").route, // Dynamic route
             arguments = listOf(navArgument("userId") { type = NavType.StringType })
         ) { backStackEntry ->
+            // Extract the userId from the backStackEntry arguments
             val userIdArg = backStackEntry.arguments?.getString("userId") ?: ""
             DashboardScreen(userId = userIdArg, navController = navController)
         }
 
+        // Other screens
         composable(Destinations.Income.route) { IncomeTaxScreen(navController) }
         composable(Destinations.Expense.route) { ExpenseScreen(navController) }
         composable(Destinations.TaxCredit.route) { TaxCreditScreen(navController) }
         composable(Destinations.Profile.route) { ProfileScreen(navController) }
         composable(Destinations.Settings.route) { SettingsScreen(navController) }
         composable(Destinations.AboutApp.route) { AboutAppScreen(navController) }
-        composable("tutorial") {
-            TutorialScreen(navController)
-        }
+        composable(Destinations.Tutorial.route) { TutorialScreen(navController) }
+        // Fullscreen video screen
         composable("fullscreen/{videoName}") { backStackEntry ->
+            // Extract the videoName from the backStackEntry arguments
             FullVideoScreen(
                 videoName = backStackEntry.arguments?.getString("videoName"),
                 onBack = { navController.popBackStack() }
