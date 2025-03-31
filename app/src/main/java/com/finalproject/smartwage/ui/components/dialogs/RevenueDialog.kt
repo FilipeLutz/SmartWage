be3af2247baa8903bd794ef0ccb5fa9ba13a917e
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:38:41 +0100

    Update RevenueDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/RevenueDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/RevenueDialog.kt
@@ -1,7 +1,7 @@
 package com.finalproject.smartwage.ui.components.dialogs
 
 import android.content.Intent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,8 +11,9 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
@@ -22,24 +23,34 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
 import com.finalproject.smartwage.ui.theme.DarkBlue
-import com.finalproject.smartwage.ui.theme.Red
+
+/**
+ * RevenueDialog is a Composable function that displays an AlertDialog
+ * This dialog is used to inform the user that they are about to leave the app
+ * and visit the Revenue.ie website.
+ *
+ * @param onDismiss A lambda function to be called when the dialog is dismissed
+ */
 
 @Composable
 fun RevenueDialog(
+    // Parameter to handle the dismissal of the dialog
     onDismiss: () -> Unit
 ) {
-
+    // Get the current context
     val context = LocalContext.current
-
+    // AlertDialog to show the information
     AlertDialog(
         onDismissRequest = onDismiss,
         title = {
-            Row (
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
+            // Row to arrange the title and icon
+            Row(
+                horizontalArrangement = Center,
+                verticalAlignment = CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-            ){
+            ) {
+                // Text to show the title
                 Text(
                     text = "Open Revenue Website",
                     fontSize = 22.sp,
@@ -48,30 +59,37 @@ fun RevenueDialog(
             }
         },
         text = {
-            Row (
+            // Row to show the message
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 8.dp)
-            ){
+            ) {
+                // Message text
                 Text(
                     text = "You are about to leave the app and visit the Revenue.ie",
                     fontSize = 19.sp,
                     fontWeight = SemiBold,
-                    style = TextStyle(lineHeight = 30.sp)
+                    style = TextStyle(
+                        lineHeight = 30.sp
+                    )
                 )
             }
         },
+        // Buttons to confirm or cancel the action
         confirmButton = {
-            Row (
-                horizontalArrangement = Arrangement.Center,
+            // Row to arrange the buttons
+            Row(
+                horizontalArrangement = Center,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 10.dp)
-            ){
-
+            ) {
+                // Button to cancel the action
                 TextButton(
                     onClick = { onDismiss() }
                 ) {
+                    // Text to show the cancel action
                     Text(
                         text = "CANCEL",
                         fontSize = 18.sp,
@@ -83,8 +101,10 @@ fun RevenueDialog(
 
                 Spacer(modifier = Modifier.width(20.dp))
 
+                // Button to confirm the action
                 TextButton(
                     onClick = {
+                        // Create an intent to open the Revenue website
                         val intent = Intent(Intent.ACTION_VIEW).apply {
                             data = "https://www.revenue.ie/en/home.aspx".toUri()
                         }
@@ -92,6 +112,7 @@ fun RevenueDialog(
                         onDismiss()
                     }
                 ) {
+                    // Text to show the confirm action
                     Text(
                         text = "CONTINUE",
                         fontSize = 18.sp,
