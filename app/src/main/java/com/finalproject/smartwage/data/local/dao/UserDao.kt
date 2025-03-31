370a3a3d1329195229297bc0d1cda84e22fe1dd4
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:37:16 +0100

    Update UserDao.kt

--- app/src/main/java/com/finalproject/smartwage/data/local/dao/UserDao.kt
+++ app/src/main/java/com/finalproject/smartwage/data/local/dao/UserDao.kt
@@ -1,8 +1,18 @@
 package com.finalproject.smartwage.data.local.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.finalproject.smartwage.data.local.entities.User
 import kotlinx.coroutines.flow.Flow
+
+/**
+ * UserDao is an interface that defines the data access methods for the User entity.
+ * It uses Room annotations to define the database operations.
+ *
+ * @Dao annotation indicates that this is a Data Access Object.
+ */
 
 @Dao
 // Data Access Object for User
