073230445c313937d908d0909de0997805a8f0c4
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:37:55 +0100

    Update Destinations.kt

--- app/src/main/java/com/finalproject/smartwage/navigation/Destinations.kt
+++ app/src/main/java/com/finalproject/smartwage/navigation/Destinations.kt
@@ -1,6 +1,12 @@
 package com.finalproject.smartwage.navigation
 
-// Sealed class Destinations.kt
+/**
+ * Destinations.kt
+ *
+ * This file contains the navigation destinations for the application.
+ * It defines the routes for different screens in the app.
+ */
+
 sealed class Destinations(val route: String) {
     // Root destination
     object Login : Destinations("login")
