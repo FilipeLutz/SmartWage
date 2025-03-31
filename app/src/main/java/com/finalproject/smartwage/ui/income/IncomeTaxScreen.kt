3170a0d58f71d788e5e7f31139ba8ba1a9f8b481
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:38:58 +0100

    Update IncomeTaxScreen.kt

--- app/src/main/java/com/finalproject/smartwage/ui/income/IncomeTaxScreen.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/income/IncomeTaxScreen.kt
@@ -1,7 +1,7 @@
 package com.finalproject.smartwage.ui.income
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,11 +12,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,9 +27,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.text.style.TextAlign
@@ -47,12 +48,23 @@ import com.finalproject.smartwage.viewModel.ExpenseViewModel
 import com.finalproject.smartwage.viewModel.IncomeViewModel
 import com.google.firebase.auth.FirebaseAuth
 
+/**
+ * IncomeTaxScreen is a Composable function that displays the income tax screen.
+ * It shows a list of user incomes and allows the user to add or edit their income.
+ *
+ * @param navController The NavController for navigation.
+ * @param viewModel The IncomeViewModel for managing income data.
+ * @param expenseViewModel The ExpenseViewModel for managing expense data.
+ */
+
 @Composable
 fun IncomeTaxScreen(
+    // Parameters
     navController: NavController,
     viewModel: IncomeViewModel = hiltViewModel(),
     expenseViewModel: ExpenseViewModel = hiltViewModel()
 ) {
+    // State variables
     var showPayslipForm by remember { mutableStateOf(false) }
     var editingIncome by remember { mutableStateOf<Income?>(null) }
     var showFab by remember { mutableStateOf(true) }
@@ -63,12 +75,14 @@ fun IncomeTaxScreen(
     LaunchedEffect(userId) {
         viewModel.loadIncomes()
     }
-
+    // Scaffold to provide the main structure of the screen
     Scaffold(
         topBar = { DashboardTopBar(navController) },
         bottomBar = { DashboardBottomBar(navController) },
         floatingActionButton = {
+            // Floating Action Button (FAB) to add a new income
             if (showFab) {
+                // FAB to add a new income
                 FloatingActionButton(
                     onClick = {
                         showPayslipForm = true
@@ -79,70 +93,80 @@ fun IncomeTaxScreen(
                         .padding(end = 4.dp)
                         .size(60.dp),
                     containerColor = DarkBlue,
-                    contentColor = Color.White,
+                    contentColor = White,
                 ) {
+                    // Icon inside the FAB
                     Icon(
-                        Icons.Default.Add,
+                        Default.Add,
                         contentDescription = "Add Income",
                     )
                 }
             }
         }
     ) { paddingValues ->
+        // Surface to provide a background color
         Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .background(MaterialTheme.colorScheme.background)
+                .background(colorScheme.background)
         ) {
+            // column to hold the content
             Column(
                 modifier = Modifier
                     .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = CenterHorizontally
             ) {
 
                 Spacer(modifier = Modifier.height(10.dp))
 
+                // Row to display the title
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+                    horizontalArrangement = Center
                 ) {
 
+                    // Title text
                     Text(
                         text = "Income Tax",
                         fontSize = 35.sp,
                         fontWeight = Bold,
-                        color = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier.padding(16.dp)
+                        color = colorScheme.primary,
+                        modifier = Modifier
+                            .padding(16.dp)
                     )
                 }
 
                 Spacer(modifier = Modifier.height(10.dp))
 
                 if (userIncomes.isEmpty()) {
-                    // Show message when there are no incomes
+                    // Column to show a message when there are no incomes
                     Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Center,
+                        horizontalAlignment = CenterHorizontally
                     ) {
+                        // Message text
                         Text(
                             text = "Click on the \"+\" button to start adding your income.",
                             fontSize = 25.sp,
                             fontWeight = SemiBold,
                             textAlign = TextAlign.Center,
-                            modifier = Modifier.padding(16.dp)
+                            modifier = Modifier
+                                .padding(16.dp)
                         )
                     }
                 } else {
-                    // Show incomes
+                    // LazyColumn to display the list of incomes
                     LazyColumn(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(horizontal = 16.dp)
                     ) {
                         items(userIncomes.size) { index ->
+                            // Display each income using the IncomeListCard
                             val income = userIncomes[index]
                             IncomeListCard(
                                 income = income,
@@ -167,16 +191,20 @@ fun IncomeTaxScreen(
                 Box(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color.Black.copy(alpha = 0.7f))
+                        .background(
+                            Black.copy(alpha = 0.7f)
+                        )
                 ) {
+                    // Column to hold the PayslipFormDialog
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
+                        horizontalAlignment = CenterHorizontally,
+                        verticalArrangement = Center
                     ) {
 
+                        // Show the PayslipFormDialog
                         PayslipFormDialog(
                             viewModel = viewModel,
                             incomeToEdit = editingIncome,
