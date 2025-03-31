0408cb0c10e17f014b8c5a31a391bcbf8f812173
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:38:39 +0100

    Update PhotoDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/PhotoDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/PhotoDialog.kt
@@ -1,20 +1,22 @@
 package com.finalproject.smartwage.ui.components.dialogs
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Absolute.SpaceBetween
+import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Default
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
@@ -22,31 +24,46 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.finalproject.smartwage.ui.theme.DarkBlue
 
+/**
+ * A dialog that allows the user to choose between taking a new photo or selecting one from the gallery.
+ *
+ * @param onDismiss Callback function to be invoked when the dialog is dismissed.
+ * @param onCameraClick Callback function to be invoked when the camera option is clicked.
+ * @param onGalleryClick Callback function to be invoked when the gallery option is clicked.
+ */
+
 @Composable
 fun PhotoDialog(
+    // Parameters
     onDismiss: () -> Unit,
     onCameraClick: () -> Unit,
     onGalleryClick: () -> Unit
 ) {
+    // AlertDialog to show the options
     AlertDialog(
         onDismissRequest = onDismiss,
         title = {
+            // Row to display the title and close button
             Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = SpaceBetween,
+                verticalAlignment = CenterVertically
             ) {
+                // Title text
                 Text(
                     "Choose an option",
                     fontSize = 24.sp,
                     fontWeight = Bold
                 )
 
+                // Close button
                 IconButton(
                     onClick = onDismiss
                 ) {
+                    // Close icon
                     Icon(
-                        imageVector = Icons.Default.Close,
+                        imageVector = Default.Close,
                         contentDescription = "Close",
                         tint = DarkBlue,
                         modifier = Modifier
@@ -56,6 +73,7 @@ fun PhotoDialog(
             }
         },
         text = {
+            // Description text
             Text(
                 "Would you like to take a new picture or choose from the gallery?",
                 fontSize = 15.sp,
@@ -63,42 +81,51 @@ fun PhotoDialog(
             )
         },
         confirmButton = {
+            // Column to display the buttons
             Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = spacedBy(8.dp),
+                horizontalAlignment = CenterHorizontally
             ) {
+                // Button to take a photo
                 Button(
                     onClick = {
                         onDismiss()
                         onCameraClick()
                     },
-                    colors = ButtonDefaults.buttonColors(DarkBlue),
+                    colors = buttonColors(DarkBlue),
                     modifier = Modifier
                         .fillMaxWidth()
                 ) {
-                    Text("Take Photo",
+                    // Text for the button
+                    Text(
+                        "Take Photo",
                         fontSize = 18.sp,
                         fontWeight = Bold
                     )
                 }
 
+                // Button to choose from the gallery
                 Button(
                     onClick = {
                         onDismiss()
                         onGalleryClick()
                     },
-                    colors = ButtonDefaults.buttonColors(DarkBlue),
+                    colors = buttonColors(DarkBlue),
                     modifier = Modifier
                         .fillMaxWidth()
                 ) {
-                    Text("Choose from Gallery",
+                    // Text for the button
+                    Text(
+                        "Choose from Gallery",
                         fontSize = 18.sp,
                         fontWeight = Bold
                     )
                 }
             }
         },
+        // Dismiss button
         dismissButton = {}
     )
 }
