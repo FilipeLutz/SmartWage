ed1e37d9a69b1a14455abdba45a17aaad6a233ae
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:38:23 +0100

    Update DeleteAccountDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/DeleteAccountDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/DeleteAccountDialog.kt
@@ -1,30 +1,40 @@
 package com.finalproject.smartwage.ui.components.dialogs
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.unit.sp
 import com.finalproject.smartwage.ui.theme.DarkBlue
-import com.finalproject.smartwage.ui.theme.Red
+
+/**
+ * DeleteAccountDialog is a Composable function that displays a dialog for confirming account deletion.
+ *
+ * @param onDismiss Callback function to be called when the dialog is dismissed.
+ * @param onConfirm Callback function to be called when the delete action is confirmed.
+ */
 
 @Composable
 fun DeleteAccountDialog(
+    // Parameters
     onDismiss: () -> Unit,
     onConfirm: () -> Unit
 ) {
+    // Delete Account Dialog
     AlertDialog(
         onDismissRequest = {},
         title = {
+            // Row for the title
             Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                horizontalArrangement = Center,
+                verticalAlignment = CenterVertically
             ) {
                 Text(
                     "Delete Account",
@@ -35,21 +45,27 @@ fun DeleteAccountDialog(
             }
         },
         text = {
+            // Text for the dialog
             Text(
                 "Are you sure you want to delete your account?",
                 fontSize = 21.sp,
                 fontWeight = SemiBold,
-                style = TextStyle(lineHeight = 22.sp)
+                style = TextStyle(
+                    lineHeight = 22.sp
+                )
             )
         },
         confirmButton = {
+            // Button to confirm the delete
             TextButton(
                 onClick = {
                     onConfirm()
                     onDismiss()
                 }
             ) {
-                Text("DELETE",
+                // Text for the confirm button
+                Text(
+                    "DELETE",
                     color = Red,
                     fontSize = 20.sp,
                     fontWeight = Bold
@@ -57,8 +73,13 @@ fun DeleteAccountDialog(
             }
         },
         dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text("CANCEL",
+            // Button to dismiss the dialog
+            TextButton(
+                onClick = onDismiss
+            ) {
+                // Text for the dismiss button
+                Text(
+                    "CANCEL",
                     color = DarkBlue,
                     fontSize = 20.sp,
                     fontWeight = Bold
