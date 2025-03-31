d3eb2a03b8bf4c0d7987c372130e035e1ecbe47d
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:39:03 +0100

    Update SettingsScreen.kt

--- app/src/main/java/com/finalproject/smartwage/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/settings/SettingsScreen.kt
@@ -6,7 +6,9 @@ import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
+import androidx.compose.foundation.layout.Arrangement.Absolute.SpaceBetween
+import androidx.compose.foundation.layout.Arrangement.End
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,11 +20,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.SwitchDefaults.colors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -30,9 +32,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -48,60 +53,79 @@ import com.finalproject.smartwage.ui.components.DashboardBottomBar
 import com.finalproject.smartwage.ui.components.DashboardTopBar
 import com.finalproject.smartwage.ui.components.dialogs.RevenueDialog
 import com.finalproject.smartwage.ui.theme.DarkBlue
-import com.finalproject.smartwage.ui.theme.Red
-import com.finalproject.smartwage.ui.theme.White
 import com.finalproject.smartwage.viewModel.AuthViewModel
 import com.finalproject.smartwage.viewModel.SettingsViewModel
 
+/**
+ * SettingsScreen is a Composable function that displays the settings screen of the app.
+ * It includes options for language selection, dark mode, profile settings, notifications,
+ * app tutorial, revenue information, and logout functionality.
+ *
+ * @param navController The NavController used for navigation between screens.
+ * @param authViewModel The AuthViewModel used for authentication-related operations.
+ * @param viewModel The SettingsViewModel used for managing settings-related operations.
+ */
+
 @Composable
 fun SettingsScreen(
+    // Parameters
     navController: NavController,
     authViewModel: AuthViewModel = hiltViewModel(),
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
 
+    // State variables
     var showRevenueDialog by remember { mutableStateOf(false) }
     val context = LocalContext.current
 
-    // Observe settings
+    // Collecting the dark mode state from the ViewModel
     val darkModeEnabled by viewModel.darkModeEnabled.collectAsState()
 
+    // Scaffold to provide the top bar and bottom bar
     Scaffold(
         topBar = { DashboardTopBar(navController) },
         bottomBar = { DashboardBottomBar(navController) }
     ) { paddingValues ->
+        // Surface to provide a background color and padding
         Surface(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-                .background(MaterialTheme.colorScheme.background)
+                .background(colorScheme.background)
         ) {
+            // Column to arrange the settings items vertically
             Column(
                 modifier = Modifier
                     .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = CenterHorizontally
             ) {
 
                 Spacer(modifier = Modifier.height(10.dp))
 
+                // Row to display the title "Settings"
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+                    horizontalArrangement = Center
                 ) {
 
+                    // Title text
                     Text(
                         text = "Settings",
                         fontSize = 35.sp,
                         fontWeight = Bold,
-                        color = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier.padding(16.dp)
+                        color = colorScheme.primary,
+                        modifier = Modifier
+                            .padding(16.dp)
                     )
                 }
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Column(modifier = Modifier.fillMaxSize()
+                // Column to display the settings items
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
 
                     HorizontalDivider()
@@ -113,10 +137,12 @@ fun SettingsScreen(
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
-                        Row (
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.End
-                        ){
+                        // Row to display the selected language
+                        Row(
+                            verticalAlignment = CenterVertically,
+                            horizontalArrangement = End
+                        ) {
+                            // Language Text
                             Text(
                                 text = "EN",
                                 fontSize = 20.sp,
@@ -137,14 +163,16 @@ fun SettingsScreen(
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
+                        // Switch to toggle dark mode
                         Switch(
-                            colors = SwitchDefaults.colors(
+                            colors = colors(
                                 checkedThumbColor = White,
                                 checkedTrackColor = DarkBlue,
                                 uncheckedThumbColor = DarkBlue,
                             ),
                             checked = darkModeEnabled,
                             onCheckedChange = { enabled ->
+                                // Update the dark mode state in the ViewModel
                                 viewModel.updateDarkModeEnabled(enabled)
                             }
                         )
@@ -155,16 +183,23 @@ fun SettingsScreen(
                     // Profile setting
                     SettingItem(
                         title = "Profile",
-                        icon = painterResource(id = R.drawable.user),
-                        onClick = { navController.navigate(Destinations.Profile.route) },
+                        icon = painterResource(id = R.drawable.profile),
+                        onClick = {
+                            // Navigate to the profile settings screen
+                            navController.navigate(
+                                Destinations.Profile.route
+                            )
+                        },
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
+                        // Icon to navigate to profile settings
                         Icon(
                             painter = painterResource(id = R.drawable.arrowforward),
                             contentDescription = "Profile Settings",
                             tint = DarkBlue,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier
+                                .size(35.dp)
                         )
                     }
 
@@ -174,15 +209,20 @@ fun SettingsScreen(
                     SettingItem(
                         title = "Notifications",
                         icon = painterResource(id = R.drawable.notifications),
-                        onClick = { openNotificationSettings(context) },
+                        onClick = {
+                            // Open notification settings
+                            openNotificationSettings(context)
+                        },
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
+                        // Icon to navigate to notification settings
                         Icon(
                             painter = painterResource(id = R.drawable.arrowforward),
                             contentDescription = "Notification Settings",
                             tint = DarkBlue,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier
+                                .size(35.dp)
                         )
                     }
 
@@ -192,7 +232,12 @@ fun SettingsScreen(
                     SettingItem(
                         title = "App Tutorial",
                         icon = painterResource(id = R.drawable.tutorial),
-                        onClick = { navController.navigate(Destinations.Tutorial.route) },
+                        onClick = {
+                            // Navigate to the tutorial screen
+                            navController.navigate(
+                                Destinations.Tutorial.route
+                            )
+                        },
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
@@ -200,24 +245,31 @@ fun SettingsScreen(
                             painter = painterResource(id = R.drawable.arrowforward),
                             contentDescription = "Go to Tutorial",
                             tint = DarkBlue,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier
+                                .size(35.dp)
                         )
                     }
 
                     HorizontalDivider()
 
+                    // Go to Revenue.ie
                     SettingItem(
                         title = "Revenue IE",
                         icon = painterResource(id = R.drawable.faq),
-                        onClick = { showRevenueDialog = true },
+                        onClick = {
+                            // Show the revenue dialog
+                            showRevenueDialog = true
+                        },
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
+                        // Icon to navigate to Revenue.ie
                         Icon(
                             painter = painterResource(id = R.drawable.arrowforward),
                             contentDescription = "Revenue.ie",
                             tint = DarkBlue,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier
+                                .size(35.dp)
                         )
                     }
 
@@ -227,45 +279,61 @@ fun SettingsScreen(
                     SettingItem(
                         title = "About App",
                         icon = painterResource(id = R.drawable.info),
-                        onClick = { navController.navigate(Destinations.AboutApp.route) },
+                        onClick = {
+                            // Navigate to the about app screen
+                            navController.navigate(
+                                Destinations.AboutApp.route
+                            )
+                        },
                         titleColor = DarkBlue,
                         iconColor = DarkBlue
                     ) {
+                        // Icon to navigate to about app
                         Icon(
                             painter = painterResource(id = R.drawable.arrowforward),
                             contentDescription = "About the App",
                             tint = DarkBlue,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier
+                                .size(35.dp)
                         )
                     }
 
                     HorizontalDivider()
 
-                    // logout
+                    // Log out option
                     SettingItem(
                         title = "Log Out",
                         icon = painterResource(id = R.drawable.logout),
                         onClick = {
+                            // Log out the user
                             authViewModel.logout()
+                            // Navigate to the login screen
                             navController.navigate(Destinations.Login.route) {
+                                // Clear the back stack
                                 popUpTo(Destinations.Login.route) { inclusive = true }
                             }
                         },
                         titleColor = Red,
                         iconColor = Red
                     ) {
+                        // Icon to log out
                         Icon(
                             painter = painterResource(id = R.drawable.arrowforward),
                             contentDescription = "Logout",
                             tint = Red,
-                            modifier = Modifier.size(35.dp)
+                            modifier = Modifier
+                                .size(35.dp)
                         )
                     }
                     HorizontalDivider()
                 }
+                // Show the revenue dialog if the state is true
                 if (showRevenueDialog) {
                     RevenueDialog(
-                        onDismiss = { showRevenueDialog = false },
+                        onDismiss = {
+                            // Dismiss the dialog
+                            showRevenueDialog = false
+                        },
                     )
                 }
             }
@@ -273,8 +341,21 @@ fun SettingsScreen(
     }
 }
 
+/**
+ * SettingItem is a Composable function that represents a single item in the settings screen.
+ * It includes an icon, title, and an optional content area for additional actions.
+ *
+ * @param title The title of the setting item.
+ * @param icon The icon to be displayed next to the title.
+ * @param titleColor The color of the title text.
+ * @param iconColor The color of the icon.
+ * @param onClick The action to be performed when the item is clicked.
+ * @param content The additional content to be displayed in the item.
+ */
+
 @Composable
 fun SettingItem(
+    // Parameters
     title: String,
     icon: Painter,
     titleColor: Color,
@@ -282,6 +363,7 @@ fun SettingItem(
     onClick: () -> Unit = {},
     content: @Composable (() -> Unit)
 ) {
+    // Row to display the setting item
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -291,24 +373,28 @@ fun SettingItem(
                 indication = null,
                 interactionSource = remember { MutableInteractionSource() }
             ) {
+                // Perform the click action
                 onClick()
             },
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = CenterVertically,
+        horizontalArrangement = SpaceBetween,
     ) {
+        // Row to display the icon and title
         Row(
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = CenterVertically
         ) {
+            // Icon
             Icon(
                 icon,
                 contentDescription = title,
                 tint = iconColor,
                 modifier = Modifier
                     .size(26.dp),
-                )
+            )
 
             Spacer(modifier = Modifier.width(10.dp))
 
+            // Title text
             Text(
                 title,
                 fontSize = 20.sp,
@@ -316,20 +402,36 @@ fun SettingItem(
                 color = titleColor
             )
         }
+        // Additional content area
         content()
     }
 }
 
-private fun openNotificationSettings(context: Context) {
+/**
+ * openNotificationSettings is a function that opens the notification settings for the app.
+ *
+ * @param context The context used to start the activity.
+ */
+
+private fun openNotificationSettings(
+    // Parameters
+    context: Context
+) {
+    // Create an intent to open the notification settings
     val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // For Android O and above, use the app notification settings
         Intent(android.provider.Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+            // Set the package name of the app
             putExtra("android.provider.extra.APP_PACKAGE", context.packageName)
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
     } else {
+        // For Android versions below O, use the general settings
         Intent(android.provider.Settings.ACTION_SETTINGS).apply {
+            // Set the action to open the settings
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
     }
+    // Start the activity to open the notification settings
     context.startActivity(intent)
 }
