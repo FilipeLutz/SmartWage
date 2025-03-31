aa18ba48f88d6453f1626ec27d0f05dc1d844e76
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:37:23 +0100

    Update Settings.kt

--- app/src/main/java/com/finalproject/smartwage/data/local/entities/Settings.kt
+++ app/src/main/java/com/finalproject/smartwage/data/local/entities/Settings.kt
@@ -3,7 +3,12 @@ package com.finalproject.smartwage.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// Settings Entity
+/**
+ * Settings is a data class that represents the settings of the application.
+ * It includes properties such as userId, notificationsEnabled,
+ * selectedLanguage, and darkModeEnabled.
+ */
+
 @Entity(tableName = "settings")
 // Data class for Settings
 data class Settings(
