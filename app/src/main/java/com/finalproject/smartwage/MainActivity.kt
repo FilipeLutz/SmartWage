80a612c3eb71ce24516afb232322b55fb02958e5
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 22:41:31 +0100

    Update MainActivity.kt

--- app/src/main/java/com/finalproject/smartwage/MainActivity.kt
+++ app/src/main/java/com/finalproject/smartwage/MainActivity.kt
@@ -8,14 +8,23 @@ import com.finalproject.smartwage.navigation.AppNavigation
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * MainActivity serves as the entry point for the application.
+ * It initializes Firebase Authentication and sets up the navigation controller.
+ *
+ * @AndroidEntryPoint annotation is used for Hilt dependency injection.
+ */
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Initialize Firebase Authentication
         val auth = FirebaseAuth.getInstance()
+        // Check if the user is logged in
         val isUserLoggedIn = auth.currentUser != null
-
+        // Initialize the navigation controller
         setContent {
             val navController = rememberNavController()
             AppNavigation(
