412926eea44f15ac27bc4fc05bb9f3465f6362aa
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:38:34 +0100

    Update PasswordErrorDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/PasswordErrorDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/PasswordErrorDialog.kt
@@ -5,14 +5,31 @@ import androidx.compose.ui.window.Dialog
 import com.finalproject.smartwage.ui.components.cards.PasswordErrorCard
 import com.finalproject.smartwage.utils.PasswordValidationError
 
+/**
+ * PasswordErrorDialog is a Composable function that displays a dialog with password validation errors.
+ *
+ * @param errors A list of password validation errors to be displayed in the dialog.
+ * @param onDismiss A callback function to be called when the dialog is dismissed.
+ */
+
 @Composable
 fun PasswordErrorDialog(
+    // Parameters:
     errors: List<PasswordValidationError>,
     onDismiss: () -> Unit
 ) {
-    if (errors.isNotEmpty()) {
-        Dialog(onDismissRequest = onDismiss) {
-            PasswordErrorCard(errors = errors, onDismiss = onDismiss)
+    // If there are any errors, show the dialog.
+    if (errors.isNotEmpty()
+    ) {
+        // Dialog that will be displayed when there are password validation errors.
+        Dialog(
+            onDismissRequest = onDismiss
+        ) {
+            // Display the PasswordErrorCard with the list of errors.
+            PasswordErrorCard(
+                errors = errors,
+                onDismiss = onDismiss
+            )
         }
     }
 }
