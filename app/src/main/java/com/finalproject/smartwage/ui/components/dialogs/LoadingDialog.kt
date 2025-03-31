7a030e90a5ebd274adc68ef087b210049ad2f68e
Filipe Lutz <144668397+FilipeLutz@users.noreply.github.com>
2025-03-31 19:38:30 +0100

    Update LoadingDialog.kt

--- app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/LoadingDialog.kt
+++ app/src/main/java/com/finalproject/smartwage/ui/components/dialogs/LoadingDialog.kt
@@ -6,31 +6,49 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
+/**
+ * LoadingDialog is a Composable function that displays a loading dialog with a circular progress indicator.
+ * It is used to indicate that a background task is in progress.
+ *
+ * @param isLoading A Boolean value that controls the visibility of the dialog.
+ */
+
 @Composable
-fun LoadingDialog(isLoading: Boolean) {
+fun LoadingDialog(
+    // Parameter to control the visibility of the dialog
+    isLoading: Boolean
+) {
+    // If isLoading is true, show the dialog
     if (isLoading) {
+        // Create a dialog
         Dialog(
             onDismissRequest = {}
         ) {
+            // Box to hold the circular progress indicator
             Box(
-                contentAlignment = Alignment.Center,
+                contentAlignment = Center,
                 modifier = Modifier
-                    .background(Color.Transparent)
+                    .background(
+                        Transparent
+                    )
             ) {
                 Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
+                    contentAlignment = Center,
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
+                    // Circular progress indicator
                     CircularProgressIndicator(
                         modifier = Modifier
                             .size(100.dp),
-                        color = Color.White
+                        color = White
                     )
                 }
             }
